c7b30d7050eaa8d8ba58063247a8059b49cca479
geovi77 <geovime1977@gmail.com>
2026-02-25 11:08:04 -0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # CONFIGURAÇÃO GEMINI
 # =========================
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.0-pro")
 
 # =========================
 # CONFIGURAÇÃO GOOGLE SHEETS
